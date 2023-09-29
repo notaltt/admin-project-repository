052811c77d1f8aa570b87cfd7d11a9135d622278
knee-son <okaokaloid@gmail.com>
2023-09-29 18:22:49 +0800

    show if incorrect credentials

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,8 +12,6 @@ export default function Login() {
 
   const handleSignUp = (e) => {
     e.preventDefault();
-    console.log(email);
-    console.log(password);
 
     const firebaseConfig = {
       apiKey: "AIzaSyBIEnvAR4FU3S-_U0kbZ_5-Ey8FdbOldvo",
@@ -39,18 +37,30 @@ export default function Login() {
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
+
+        // Check if the error code indicates incorrect credentials
+        if (errorCode === 'auth/wrong-password' || errorCode === 'auth/user-not-found') {
+          // Display the modal with the error message
+          const modal = document.getElementById('modal');
+          const errorMessageElement = document.getElementById('error-message');
+          errorMessageElement.textContent = 'Credentials are incorrect. Please try again.';
+          modal.classList.remove('hidden');
+
+          // Add a click event listener to close the modal
+          const closeModalButton = document.getElementById('close-modal');
+          closeModalButton.addEventListener('click', () => {
+            modal.classList.add('hidden');
+          });
+        }
       });
   };
 
   return (
-    <>
-      <div className="flex relative flex-1 h-screen dark:bg-gray-900 bg-white flex-col px-6 py-12 lg:px-8">
-           <div className='absolute right-5 top-5'>
-           <DarkMode/> 
-           </div>
-                      
+    <div className="flex relative flex-1 h-screen dark:bg-gray-900 bg-white flex-col px-6 py-12 lg:px-8">
+      <div className='absolute right-5 top-5'>
+        <DarkMode/> 
+        </div>
+                  
         <div className='mt-20 sm:mt-28'>
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
@@ -59,7 +69,7 @@ export default function Login() {
             alt="Privo Logo"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight dark:text-white text-gray-900">
-            Sign in to your account
+            Welcome to Privo, admin!
           </h2>
         </div>
 
@@ -78,7 +88,7 @@ export default function Login() {
                   required
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 pl-2"
                 />
               </div>
             </div>
@@ -89,9 +99,6 @@ export default function Login() {
                   Password
                 </label>
                 <div className="text-sm">
-                  <a href="login" className="font-semibold text-blue-600 dark:text-purple-400 hover:text-blue-500">
-                    Forgot password?
-                  </a>
                 </div>
               </div>
               <div className="mt-2">
@@ -103,7 +110,7 @@ export default function Login() {
                   required
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 pl-2"
                 />
               </div>
             </div>
@@ -117,19 +124,18 @@ export default function Login() {
               </button>
             </div>
           </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
-            <a href="/dashboard" className="font-semibold leading-6 dark:text-purple-400 text-blue-600 hover:text-blue-500">
-              Register now.
-            </a>
-          </p>
         </div>
-        </div>
-        
       </div>
-      
-    </>
+
+      <div id="modal" class="fixed top-0 left-0 w-full h-full bg-opacity-75 bg-gray-900 flex hidden justify-center items-center">
+          <div class="bg-white rounded-lg shadow-lg p-8">
+              <h2 class="text-xl font-semibold mb-4">Error</h2>
+              <p id="error-message">Credentials are incorrect. Please try again.</p>
+              <button id="close-modal" class="mt-4 bg-purple-400 hover:bg-purple-500 text-white font-semibold px-4 py-2 rounded">Close</button>
+          </div>
+      </div>
+
+    </div>
   )
 }
   
